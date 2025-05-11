2a1702322b03b9548c794a012d28309a1b9e4596
Rihamhanifa <rihamahamed625@gmail.com>
2025-05-11 18:04:26 +0530

    Add files via upload

---
+++ Mars-Tech Login & SignUp Form with Data base connect/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Responsive Login Form</title>
+ <link rel="stylesheet" href="./login_style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+
+<div class="login">
+  <div class="login-header">
+    <h1>Login</h1>
+  </div>
+  <div class="login-form">
+    <form action="login_process.php" method="post" enctype="multipart/form-data">
+      <h3>Username:</h3>
+      <input type="text"  name="name" placeholder="Username"/><br>
+      <h3>Password:</h3>
+      <input type="password" name="password" placeholder="Password"/>
+      <br>
+      <input type="submit" name="submit" value="Login" class="login-button"/>
+    </form>
+    <br>
+    <a class="sign-up" >
+    <a href="signup.php">
+    Sign Up!</a>
+    <br>
+  </div>
+</div>
+<div class="error-page">
+  <div class="try-again">Error: Try again?</div>
+</div>
+<!-- partial -->
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script><script  src="./script.js"></script>
+
+</body>
+</html>
---
+++ Mars-Tech Login & SignUp Form with Data base connect/login_process.php
@@ -0,0 +1,38 @@
+<?php
+    include("config.php");
+
+    session_start();
+    if(isset($_POST['submit'])){
+		$name=$_POST["name"];
+		$password=$_POST["password"];
+
+        if(empty($name)){
+            header("location:login.php?error=User Name is Required");
+            exit();
+        }elseif(empty($password)){
+            header("location:login.php?error=Password is Required");
+            exit();
+        }
+        else{
+            $sql= "SELECT * FROM users WHERE  name='$name' AND password='$password'" ;
+            $result= mysqli_query($mysqli,$sql);
+
+            if(mysqli_fetch_assoc($result))
+            {
+                $row=mysqli_fetch_assoc($result);
+                $_SESSION['name']=$row["name"];
+                $_SESSION['id']=$row["id"];
+                header("Location:index.php");
+                exit();
+            }
+            else
+            {
+                header("Location:login.php?error=invalid username or password");
+                exit();
+            }    
+        }          
+    
+
+    }
+    
+?>
